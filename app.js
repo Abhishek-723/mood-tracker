fa35934abc69c069f73e785609396909d1f83dec
Abhishek <abhishekpradhan931@gmail.com>
2021-08-27 21:14:05 +0530

    Keep a track on your mood

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const moodInput = document.getElementById("cars");
+const addButton = document.getElementById("submit");
+const moodList = document.querySelector(".mood-list");
+
+
+addButton.addEventListener("click", addMood);
+
+function addMood(event){
+    console.log('clicked');
+    event.preventDefault();
+
+    const boxLi = document.createElement("li");
+    boxLi.classList.add("boxes");
+    const newMood = document.createElement("div");
+    newMood.classList.add("mood");
+    newMood.innerHTML=moodInput.value;
+    const moodText = document.createElement("div");
+    moodText.classList.add("body");
+    if(moodInput.value==="Bad"){
+        moodText.innerHTML = "In the middle of difficulties lie oppurtunities, work your way to a better future..."
+    }
+    if(moodInput.value==="Decent"){
+        newMood.style.color = "rgb(185, 183, 16)";
+        moodText.innerHTML = "Allow your passion to become purpose and one day it will become your profession..."
+    }
+    if(moodInput.value==="Bad"){
+        moodText.innerHTML = "In the middle of difficulties lie oppurtunities, work your way to a better future..."
+    }
+    if(moodInput.value==="Good"){
+        newMood.style.color = "rgb(27, 233, 27)";
+        moodText.innerHTML = "Once you accept your limits, you can't go beyond. So, keep pushing youself to the fullest..."
+    }
+    boxLi.appendChild(newMood);
+    boxLi.appendChild(moodText);
+    if(moodInput.value!==""){ 
+        moodList.appendChild(boxLi);
+    }
+}
+
